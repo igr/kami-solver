a455014a48ad88250d007ba63358dc085b526e16
Emilija Gregoric <Emilija.Gregoric@zuehlke.com>
2019-10-06 23:13:51 +0200

    Clone and reduce

--- src/main/kotlin/com/oblac/kami/model/Board.kt
+++ src/main/kotlin/com/oblac/kami/model/Board.kt
@@ -3,8 +3,22 @@ package com.oblac.kami.model
 class Board(all: List<Tile>) {
 
     private val allTiles = all.toSet()
+    private var parentBoard: Board? = null
 
     fun forEach(tileConsumer: (Tile) -> Unit) {
         allTiles.forEach(tileConsumer)
+    }
+
+    fun clone(): Board {
+        var newBoard = Board(allTiles.toList())
+        newBoard.parentBoard = this
+        return newBoard
+    }
+
+    fun reduce() {
+        var tilesToReduce = allTiles.toMutableSet()
+        while (tilesToReduce.isNotEmpty()){
+            tilesToReduce = tilesToReduce.first().incorporateAllNeighbours(tilesToReduce)
+        }
     }
 }
--- src/main/kotlin/com/oblac/kami/model/Tile.kt
+++ src/main/kotlin/com/oblac/kami/model/Tile.kt
@@ -12,6 +12,30 @@ data class Tile(
         neighbour.connections.add(this)
     }
 
+    fun incorporateAllNeighbours(tilesToReduce: MutableSet<Tile>): MutableSet<Tile> {
+        var secondStepConnections = mutableSetOf<Tile>()
+        var disappearingTiles = mutableSetOf<Tile>()
+        connections.forEach{
+            if(it.color == this.color) {
+                secondStepConnections.addAll(it.connections)
+                it.disappear()
+                disappearingTiles.add(it)
+            }
+        }
+        connections.addAll(secondStepConnections)
+
+        tilesToReduce.removeAll(disappearingTiles)
+        tilesToReduce.remove(this)
+        return tilesToReduce
+    }
+
+    private fun disappear() {
+        connections.forEach{
+            it.connections.remove(it)
+            // treba li jos odnekud da ga obrisem?
+        }
+    }
+
     override fun toString(): String {
         var s = ""
         connections.forEach { s += "${it.color} " }
