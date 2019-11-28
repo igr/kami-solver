282d7c05a1a766d24d329991c80b362e4417dfe5
Igor Spasić <igor.spasic@gmail.com>
2019-11-28 10:06:32 +0100

    fixing bug

--- src/main/kotlin/com/oblac/kami/ImageParser.kt
+++ src/main/kotlin/com/oblac/kami/ImageParser.kt
@@ -5,22 +5,13 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 import kotlin.math.abs
-import kotlin.math.sqrt
 
 class ImageParser {
 
-	private val usedRgbColors = mutableListOf<Int>()
-
-	init {
-		usedRgbColors.add(0xffffff)
-	}
+	private val usedColors = mutableListOf<Int>()
 
 	fun processImage(imageFile: File, visitor: TilesVisitor) {
 		val image: BufferedImage = ImageIO.read(imageFile)
-
-		collectAllColors(image)
-		println("Total colors: ${usedRgbColors.size - 1}")
-
 		val g = image.graphics
 
 		val w = image.width
@@ -38,24 +29,17 @@ class ImageParser {
 		while (y < maxY) {
 			while (x < w) {
 				val pixel = image.getRGB(x, y)
-				val color = identifyColor(pixel, x, y)
+				val tile = visitor.visitTile(colorOf(pixel))
 
-				if (color == 0) {
-					visitor.visitEmptyTile()
-				} else {
-					// tile exist
-					val tile = visitor.visitTile(color)
+				image.setRGB(x, y, pixel xor 0xFFFFFF)
 
-					image.setRGB(x, y, pixel xor 0xFFFFFF)
-
-					val str = "(${tile.x},${tile.y}):${tile.color}"
-					val fm = g.fontMetrics
-					val rect = fm.getStringBounds(str, g)
-					g.color = Color.BLACK
-					g.fillRect(x, y - fm.ascent, rect.width.toInt(), rect.height.toInt())
-					g.color = Color.WHITE
-					g.drawString(str, x, y)
-				}
+				val str = "(${tile.x},${tile.y}):${tile.color}"
+				val fm = g.fontMetrics
+				val rect = fm.getStringBounds(str, g)
+				g.color = Color.BLACK
+				g.fillRect(x, y - fm.ascent, rect.width.toInt(), rect.height.toInt())
+				g.color = Color.WHITE
+				g.drawString(str, x, y)
 
 				x += deltaX
 			}
@@ -67,51 +51,40 @@ class ImageParser {
 		ImageIO.write(image, "PNG", File("out.png"))
 	}
 
-	/**
-	 * Tries it best to identify a color.
-	 */
-	private fun identifyColor(rgb: Int, x: Int, y: Int): Int {
-		for (threshold in 75 downTo 50 step 5) {
-			val matchedColor = usedRgbColors.indexOfFirst { isMatchingColor(rgb, it, threshold) }
-			if (matchedColor > -1) {
-				return matchedColor
-			}
+	private fun colorOf(rgb: Int): Int {
+		val matchedColor = usedColors.indexOfFirst { isMatchingColor(rgb, it) }
+		if (matchedColor > -1) {
+			return matchedColor
 		}
-		throw Exception("Color not detected at $x,$y")
-	}
-
-	private fun collectAllColors(image: BufferedImage) {
-		val deltaX = 17
-		val y = 2100
-		var x = image.width - deltaX
-
-		while (x > 350) {
-			val pixel = image.getRGB(x, y)
-			image.graphics.drawString("x", x, y)
-
-			val matchedColor = usedRgbColors.indexOfFirst { isMatchingColor(pixel, it, 75) }
-			if (matchedColor == -1) {
-				usedRgbColors.add(pixel)
-			}
-
-			x -= deltaX
-		}
+		usedColors.add(rgb)
+		return usedColors.size - 1
 	}
 
 	// Not all tiles of the same color are actually colored the same:
 	// the game has some 'brightness' overlay so top tiles are more lighter
 	private fun isMatchingColor(intColor1: Int, intColor2: Int, percent: Int = 75): Boolean {
-		val threshold = 255 - (255 / 100f * percent)
+		val threadSold = 255 - (255 / 100f * percent)
 		val color1 = Color(intColor1)
 		val color2 = Color(intColor2)
 
-		//val diffAlpha = abs(color1.alpha - color2.alpha)
+		val diffAlpha = abs(color1.alpha - color2.alpha)
 		val diffRed = abs(color1.red - color2.red)
 		val diffGreen = abs(color1.green - color2.green)
 		val diffBlue = abs(color1.blue - color2.blue)
 
-		val distance = sqrt((diffRed * diffRed + diffGreen * diffGreen + diffBlue * diffBlue).toDouble())
+		if (diffAlpha > threadSold) {
+			return false
+		}
+		if (diffRed > threadSold) {
+			return false
+		}
+		if (diffGreen > threadSold) {
+			return false
+		}
+		if (diffBlue > threadSold) {
+			return false
+		}
 
-		return distance < threshold
+		return true
 	}
 }
--- src/main/kotlin/com/oblac/kami/Run.kt
+++ src/main/kotlin/com/oblac/kami/Run.kt
@@ -7,16 +7,16 @@ data class Puzzle(val index: Int, val numberOfSteps: Int)
 val puzzles = mapOf(
 	"game-0" to Puzzle(0, 2),       // 15 ms (10 clicks)
 	"game-1" to Puzzle(1, 4),       // 120 ms (4748 clicks)
-	"game-2" to Puzzle(2, 4),       // 688 ms (6985 clicks)
-	"game-3" to Puzzle(3, 3),       // 70 ms (375 clicks)
-	"game-4" to Puzzle(4, 5),       // 487ms (11530 clicks)
+	"game-2" to Puzzle(2, 4),       // 688 ms (6353 clicks)
+	"game-3" to Puzzle(3, 3),       // 42 ms (281 clicks)
+	"game-4" to Puzzle(4, 5),       // 487ms (10922 clicks)
 
 	"hard-1" to Puzzle(99901, 7),
 	"hard-2" to Puzzle(99902, 7)
 )
 
 fun main() {
-	val puzzle = puzzles["game-1"] ?: error("Invalid puzzle name")
+	val puzzle = puzzles["game-4"] ?: error("Invalid puzzle name")
 //	val puzzle = puzzles["hard-1"] ?: error("Invalid puzzle name")
 
 	val board = BoardLoader().loadBoardFromScreenshot(puzzleScreenshotsIndex = puzzle.index)
